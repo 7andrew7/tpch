47c6985a18d64445ae9139fd7babc64af3abc156
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-05 12:13:46 -0800

    Build a gtest test

--- q1/build.sh
+++ q1/build.sh
@@ -6,5 +6,6 @@ GTEST_LIB_DIR=/scratch/gtest-1.7.0/build
 CXX=c++
 CXX_FLAGS="-g -Wall -Wextra -std=c++11"
 
-$CXX ${CXX_FLAGS}  -c q1.cpp
-$CXX ${CXX_FLAGS} -lbenchmark q1_perf.cpp -o q1_perf.exe
+$CXX $CXX_FLAGS  -c q1.cpp
+$CXX $CXX_FLAGS -lbenchmark q1_perf.cpp -o q1_perf.exe
+$CXX $CXX_FLAGS -I $GTEST_INCLUDE_DIR $GTEST_LIB_DIR/libgtest.a $GTEST_LIB_DIR/libgtest_main.a q1_test.cpp -o q1_test.exe
---
+++ q1/q1_test.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(q1, basic) {
+    EXPECT_EQ(1 + 2, 3);
+}
