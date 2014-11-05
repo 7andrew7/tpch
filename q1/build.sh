11e0e5698a3f2579bd2cb92fa0055e6680467d3e
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-05 11:57:14 -0800

    Factor out some build variables

--- q1/build.sh
+++ q1/build.sh
@@ -1,2 +1,10 @@
-c++ -g -Wall -Wextra -std=c++11  -c q1.cpp
-c++ -g -Wall -Wextra -std=c++11  q1.o -lbenchmark q1_perf.cpp -o q1_perf.exe
+#!/bin/sh
+
+GTEST_INCLUDE_DIR=/scratch/gtest-1.7.0/include
+GTEST_LIB_DIR=/scratch/gtest-1.7.0/build
+
+CXX=c++
+CXX_FLAGS="-g -Wall -Wextra -std=c++11"
+
+$CXX ${CXX_FLAGS}  -c q1.cpp
+$CXX ${CXX_FLAGS} -lbenchmark q1_perf.cpp -o q1_perf.exe
